1d79df6f15d3c43244f151c7103e93522b29700c
isaac santacruz <isaacsantacruz@mbp-de-isaac.mynetworksettings.com>
2023-05-04 15:06:41 -0400

    Where is Waldo answered

--- answers.js
+++ answers.js
@@ -67,3 +67,15 @@ console.log(lastHalf);
 console.log(favMovies);
 //////////
 console.log(favMovies.indexOf("Fast and Furious"));
+
+//////
+const whereIsWaldo = [["Timmy", "Frank"], "Eggbert",
+                    ["Lucinda", "Jacc", "Neff", "Snoop"],
+                    ["Petunia", ["Baked Goods", "Waldo"]]];
+
+///
+ whereIsWaldo.splice(1, 1); 
+ /////
+ whereIsWaldo[2][2] = "No One";
+ ///// 
+ console.log(whereIsWaldo[3][1][1]);         
